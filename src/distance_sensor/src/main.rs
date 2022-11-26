c57256f66cbdf9389e9bdff95f5c9c048b4215c3
Jon <jonathan.bihet@gmail.com>
2022-11-27 00:02:52 +0100

    add led controls

--- src/distance_sensor/src/main.rs
+++ src/distance_sensor/src/main.rs
@@ -5,13 +5,21 @@ use std::time::{Duration, Instant};
 
 mod distance_sensor;
 
+const THRESHOLD: f64 = 20.0;
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let led = LED::new(17);
     println!("---Disance Sensor Tests Program---");
     let mut sensor = distance_sensor::DistanceSensor::new(18,24).unwrap();
     loop {
         let distance = sensor.get_distance();
 
         println!("Distance: {}cm", distance);        
+        if distance >= 0.0 && distance <= THRESHOLD  {
+            led.on();
+        } else {
+            led.off();
+        }
 
         thread::sleep(Duration::from_millis(500));
     }
